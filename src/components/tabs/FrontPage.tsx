6c5b5e563005564214defa48d095e33382ca0e6f
Zelheim <ziqi2011@gmail.com>
2025-09-09 21:10:32 -0400

    Refactor FrontPage component structure for improved readability and maintainability

--- src/components/tabs/FrontPage.tsx
+++ src/components/tabs/FrontPage.tsx
@@ -15,41 +15,37 @@ const FrontPage: React.FC<FrontPageProps> = ({ onStartAssessment }) => {
       role="region" 
       aria-labelledby="front-page-title"
     >
-      <div>
-        <div className="container">
-          <div className="panel panel-info mrgn-tp-lg">
-            <div className="panel-body">
-              <p className="mrgn-tp-md">{t('frontPage.intro')}</p>
-              <p>{t('frontPage.assessmentInfo')}</p>
-              
-              <ul 
-                className="list-unstyled mrgn-tp-md" 
-                aria-label={t('frontPage.bulletPoints.label')}
-              >
-                <li className="mrgn-bttm-sm">
-                  <span className="glyphicon glyphicon-check text-success mrgn-rght-sm" aria-hidden="true"></span>
-                  {t('frontPage.bulletPoints.item1')}
-                </li>
-                <li className="mrgn-bttm-sm">
-                  <span className="glyphicon glyphicon-check text-success mrgn-rght-sm" aria-hidden="true"></span>
-                  {t('frontPage.bulletPoints.item2')}
-                </li>
-              </ul>
-              
-              <p className="mrgn-tp-md">{t('frontPage.noteToUsers')}</p>
+      <div className="panel panel-info mrgn-tp-lg">
+        <div className="panel-body">
+          <p className="mrgn-tp-md">{t('frontPage.intro')}</p>
+          <p>{t('frontPage.assessmentInfo')}</p>
+          
+          <ul 
+            className="list-unstyled mrgn-tp-md" 
+            aria-label={t('frontPage.bulletPoints.label')}
+          >
+            <li className="mrgn-bttm-sm">
+              <span className="glyphicon glyphicon-check text-success mrgn-rght-sm" aria-hidden="true"></span>
+              {t('frontPage.bulletPoints.item1')}
+            </li>
+            <li className="mrgn-bttm-sm">
+              <span className="glyphicon glyphicon-check text-success mrgn-rght-sm" aria-hidden="true"></span>
+              {t('frontPage.bulletPoints.item2')}
+            </li>
+          </ul>
+          
+          <p className="mrgn-tp-md">{t('frontPage.noteToUsers')}</p>
 
-              <p 
-                className="mrgn-tp-md"
-                dangerouslySetInnerHTML={{ __html: t('frontPage.usage') }}
-              ></p>
-              
-              <p 
-                className="mrgn-tp-md"
-                dangerouslySetInnerHTML={{ __html: t('frontPage.contactInfo') }}
-              ></p>
-              
-            </div>
-          </div>
+          <p 
+            className="mrgn-tp-md"
+            dangerouslySetInnerHTML={{ __html: t('frontPage.usage') }}
+          ></p>
+          
+          <p 
+            className="mrgn-tp-md"
+            dangerouslySetInnerHTML={{ __html: t('frontPage.contactInfo') }}
+          ></p>
+          
         </div>
       </div>
       
